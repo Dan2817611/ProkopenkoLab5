cbde78110cf5e4b99d45c34cc6e61def83fc5651
Dan2817611 <131982555+Dan2817611@users.noreply.github.com>
2023-10-11 11:09:46 +0300

    Add

--- program1.py
+++ program1.py
@@ -1,5 +1,6 @@
 import re
-def ukrainian_alphabet(word):
+
+def ukrainian_alphabet_order(word):
     ukrainian_alphabet = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя"
     return [ukrainian_alphabet.index(letter.lower()) for letter in word]
 
@@ -17,17 +18,18 @@ def process_text(filename):
             ukrainian_words = [word for word in words if re.match(r'^[А-ЯІЇЄҐа-яіїєґ]+$', word, re.UNICODE)]
             english_words = [word for word in words if re.match(r'^[a-zA-Z]+$', word)]
 
-            ukrainian_words_sorted = sorted(ukrainian_words, key=ukrainian_alphabet)
-            english_words_sorted = sorted(english_words)
+            ukrainian_words_sorted = sorted(ukrainian_words, key=ukrainian_alphabet_order)
+            english_words_sorted = sorted(english_words, key=str.casefold)
 
             if ukrainian_words_sorted:
-                print("\nУкраїнські слова:")
+                print("")
                 print(", ".join(ukrainian_words_sorted))
             else:
                 print("\nУкраїнських слів не знайдено")
 
             if english_words_sorted:
-                print(" ".join(english_words_sorted))
+                print("")
+                print(", ".join(english_words_sorted))
             else:
                 print("\nАнглійських слів не знайдено")
 
@@ -37,7 +39,5 @@ def process_text(filename):
         print(f"Файл '{filename}' не знайдено.")
     except Exception as e:
         print(f"Виникла помилка: {e}")
+
 process_text('AlpText.txt')
-
-
-
